07aec02b4c54151fa34c230afe44699b49175525
laynesquare <83596981+laynesquare@users.noreply.github.com>
2022-08-06 22:25:19 +0800

    Add files via upload

---
+++ git_practice/happyJavascript.js
@@ -0,0 +1,24 @@
+console.log('nice');
+
+console.log('nice2');
+
+console.log('cunt change');
+console.log('bitch2');
+
+console.log('dick2');
+console.log('dick5');
+
+console.log('add from local 1149');
+
+const no = 'this is from web 1146';
+const no = 'this is from web 1146';
+
+const no = 'this is from local 1219';
+
+const yes = 'this is from web 1146';
+const yes = 'this is from local 1224';
+const yes = 'this is from web 1225';
+const yes = 'this is from web 1225';
+const yes = 'haha';
+
+let so = "bad";
